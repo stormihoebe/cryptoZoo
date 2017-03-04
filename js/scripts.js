30a0e9ebe6ea6bc3659e234439f4477f0f62c8b4
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-03-03 16:56:42 -0800

    add initial js

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+//Pizza Constructor
+function Player(name, cryptid, keys, evidence, allies){
+this.name = name;
+this.cryptid = cryptid;
+this.keys = keys;
+this.evidence = evidence;
+this.allies = allies;
+};
+Player.prototype.Report = function() {
+};
+
+
+$(function(){
+  $("#userForm").submit(function(event){
+    
+
+  }) //end userForm Submit
+
+
+
+
+
+
+
+}); //end jquery

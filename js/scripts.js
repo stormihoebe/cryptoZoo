997090871d7f4fa4925145331c190ed797b41eb4
Stormi Hoebelheinrich <Stormi Hoebelheinrich>
2017-03-12 11:24:51 -0700

    collect name and cryptid type from initial survey, hide all other information from page

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,18 @@ this.allies = allies;
 };
 Player.prototype.Report = function() {
 };
+//Global variables
+var name;
+var cryptidType
 
 
 $(function(){
   $("#userForm").submit(function(event){
-    
+    event.preventDefault();
+    name = $("input#name").val();
+    cryptidType = $("#cryptidType").val();
+    console.log(name);
+    console.log(cryptidType);
 
   }) //end userForm Submit
 

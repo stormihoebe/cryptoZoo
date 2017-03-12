01515d7effe61185e4f9d986ac917297b1de801a
Stormi Hoebelheinrich <Stormi Hoebelheinrich>
2017-03-12 12:15:49 -0700

    add hide and show message buttons as well as a start game button which will take you to the first room/environment

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,8 @@ var cryptidType
 var keys = [];
 var evidence = [];
 var allies = [];
-var newPlayer
+var newPlayer;
+var started;
 
 $(function(){
   $("#userForm").submit(function(event){
@@ -29,9 +30,25 @@ $(function(){
     $("#amountOfEvidence").text(evidence.length);
     $("#amountOfAllies").text(allies.length);
     $("#message").text("Hello, " + name + ", let me fill you in on what is going on here. You are a " + cryptidType + " and now is a golden opportunity to escape CryptoZoo where you are being held captive. To escape you must find 3 peices of evidence, 2 keys, and an ally before making it to the entrance of the facility. Click through rooms and collect items. Also, try not to get caught.");
-
   }); //end userForm Submit
+  $("#getStarted").click(function(){
+    $("#cageRoom").show();
+    $("#getStarted").hide();
+    started = "yes";
+  });
 
+  $("#hideMessage").click(function(){
+    if (started === "yes") {
+      $("#messageBoard").hide();
+      $("#showMessage").show();
+    } else {
+      alert("Please click Get Started before hiding message");
+    };
+  });
+  $("#showMessage").click(function(){
+    $("#messageBoard").show();
+    $("#showMessage").hide();
+  });
 
 
 
